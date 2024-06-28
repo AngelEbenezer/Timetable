003d20bf21ea3e7bb325dc23a53297467fafecb6
AngelEbenezer <152778115+AngelEbenezer@users.noreply.github.com>
2024-06-28 20:54:44 +0530

    Delete Minipro/timetable_project directory

--- Minipro/timetable_project/timetable/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Subject, Teacher, Class, Timetable
-
-admin.site.register(Subject)
-admin.site.register(Teacher)
-admin.site.register(Class)
-admin.site.register(Timetable)
-
-
--- Minipro/timetable_project/timetable/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('generate/', views.generate_timetable, name='generate_timetable'),
-    path('api/timetables/', views.api_timetables, name='api_timetables'),
-]
